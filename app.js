e923808555af2b1374aa2e88e5e9d4b6e79036d1
vishal-9368 <vshalsha@1234@gmail.com>
2024-01-19 22:03:29 +0530

    sec

--- app.js
+++ app.js
@@ -69,7 +69,9 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   res.render("home");
 });
-
+app.get("/home1", (req, res) => {
+  res.send('ok working');
+});
 // Routes
 app.use(productRoutes);
 app.use(reviewRoutes);
